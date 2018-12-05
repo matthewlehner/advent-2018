f46ad9ddf594ad878c68613792172367b9c43cc7
Matthew Lehner <matthew@mpl.io>
2018-12-05 06:27:30 -0800

    Day 2, Part 2 (so ugly 🤷‍♂️)

--- lib/advent/day_2.ex
+++ lib/advent/day_2.ex
@@ -1,9 +1,13 @@
 defmodule Advent.Day2 do
-  @spec calculate_checksum() :: integer()
-  def calculate_checksum() do
+  def day_2_input do
     "lib/advent/day_2_input"
     |> File.read!()
     |> String.split()
+  end
+
+  @spec calculate_checksum() :: integer()
+  def calculate_checksum() do
+    day_2_input()
     |> calculate_checksum()
   end
 
@@ -31,7 +35,58 @@ defmodule Advent.Day2 do
     end)
   end
 
-  def categorize_id(value, {two_times, three_times}) when value == 2, do: {1, three_times}
-  def categorize_id(value, {two_times, three_times}) when value == 3, do: {two_times, 1}
+  def categorize_id(value, {_, three_times}) when value == 2, do: {1, three_times}
+  def categorize_id(value, {two_times, _}) when value == 3, do: {two_times, 1}
   def categorize_id(_, values), do: values
+
+  def find_common() do
+    day_2_input()
+    |> check_common()
+  end
+
+  @spec check_common([]) :: String.t() | false
+  def check_common([id | ids]) do
+    result =
+      Enum.reduce_while(ids, nil, fn compared_id, acc ->
+        case compare_ids(compared_id, id) do
+          false -> {:cont, nil}
+          string -> {:halt, string}
+        end
+      end)
+
+    if is_binary(result) do
+      result
+    else
+      check_common(ids)
+    end
+  end
+
+  def check_common([]), do: false
+
+  def compare_ids(id1, id2) do
+    id1 = String.graphemes(id1)
+    id2 = String.graphemes(id2)
+
+    [id1, id2]
+    |> List.zip()
+    |> Enum.reduce_while({[], 0}, fn {grapheme1, grapheme2}, {chars, failures} ->
+      response =
+        case check_grapheme(grapheme1, grapheme2) do
+          false -> {chars, failures + 1}
+          grapheme -> {[grapheme | chars], failures}
+        end
+
+      case response do
+        {_, 2} -> {:halt, false}
+        _ -> {:cont, response}
+      end
+    end)
+    |> case do
+      false -> false
+      {chars, _} -> chars |> Enum.reverse() |> Enum.join()
+    end
+  end
+
+  def check_grapheme(grapheme, grapheme), do: grapheme
+  def check_grapheme(_grapheme1, _grapheme2), do: false
 end
--- test/advent/day_2_test.exs
+++ test/advent/day_2_test.exs
@@ -13,4 +13,15 @@ defmodule Advent.Day2Test do
       assert Day2.calculate_checksum() == 8118
     end
   end
+
+  describe "Day2.find_id" do
+    test "gets id from multiple inventory ids" do
+      ids = ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]
+      assert Day2.compare_ids("fghij", "fguij") == "fgij"
+      assert Day2.check_common(ids) == "fgij"
+      assert Day2.check_common(["abcde", "fghij"]) == false
+
+      assert 1 == Day2.find_common()
+    end
+  end
 end
